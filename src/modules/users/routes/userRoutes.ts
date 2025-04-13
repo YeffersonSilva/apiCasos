15713ad7ff75ae1b0a9c19c8a39f9f3d03d3f5a2
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2025-04-13 13:37:36 -0300

    feat: agregar rutas de usuarios y archivo de índice para gestionar las operaciones relacionadas con los usuarios en la aplicación

---
+++ src/modules/users/routes/userRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { UserController } from "../controllers/UserController";
+
+const router = Router();
+const userController = new UserController();
+
+// Rutas de usuarios
+router.get("/users", userController.findAll.bind(userController));
+router.get("/users/:id", userController.findById.bind(userController));
+router.post("/users", userController.create.bind(userController));
+router.put("/users/:id", userController.update.bind(userController));
+router.delete("/users/:id", userController.delete.bind(userController));
+
+export default router;
